8601196d35948324a412430123acb4d4f43db94e
Nicolas Leon <nicolasnicolasleon@gmail.com>
2021-02-11 18:38:25 -0500

    commit inicial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const Pokedex = ({ name, url }) => {
+  const [pokemon, setPokemon] = useState(null);
+
+  useEffect(() => {
+    const promise = axios(url);
+
+    promise.then((res) => {
+      setPokemon(res.data.sprites.front_shiny);
+    });
+  });
+
+  return (
+    <div>
+      <img src={pokemon} alt={name} />
+      <h1>{name}</h1>
+    </div>
+  );
+};
+
+const Search = ({ handleSearch }) => {
+  const [searchTerm, setSearchTerm] = useState(""); // Candidato a useCallback y una explicacion del porque
+
+  return (
+    <div>
+      <input onChange={(e) => setSearchTerm(e.target.value)} />
+      <button onClick={() => handleSearch(searchTerm)}>Search</button>
+    </div>
+  );
+};
 
 function App() {
+  const [pokes, setPokes] = useState([]);
+  const [query, setQuery] = useState("");
+
+  // Llamar PokeAPI
+  useEffect(() => {
+    if (query) {
+      const promise = axios(`https://pokeapi.co/api/v2/type/${query}`);
+
+      promise.then((res) => {
+        setPokes(res.data.pokemon.slice(0, 10));
+      });
+    }
+  }, [query]);
+
+  useEffect(() => {
+    console.log(pokes);
+  }, [pokes]);
+
+  const handleSearchPokemons = (value) => {
+    setQuery(value);
+  };
+
+  const myArrOfPokemons = pokes.map((value) => (
+    <Pokedex
+      name={value.pokemon.name}
+      url={value.pokemon.url}
+      key={value.pokemon.name}
+    />
+  ));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pokedex</h1>
+      <Search handleSearch={handleSearchPokemons} />
+      {pokes.length > 0 && myArrOfPokemons}
     </div>
   );
 }
